07a7c21bd394cdc70064c4ad1ed63c3e45d7868e
theandersonn <theandersonn@gmail.com>
2019-08-28 23:02:31 -0300

    Estrutura inicial WP

---
+++ wp-app/local-config.php
@@ -0,0 +1,16 @@
+<?php
+
+// DATABASE
+define( 'DB_NAME', 'handel' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'db' );
+define( 'WPLANG', 'pt_BR' );
+
+$table_prefix = 'wp_';
+
+// URLS
+define( 'WP_CONTENT_URL', 'http://127.0.0.1/content' );
+
+// DEBUG
+define( 'WP_DEBUG', true );
---
+++ wp-app/login/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="robots" content="noindex, nofollow">
+		<title>Loading...</title>
+		<meta http-equiv="refresh" content="0; url=../cms/wp-admin/">
+	</head>
+	<body>
+	</body>
+</html>
