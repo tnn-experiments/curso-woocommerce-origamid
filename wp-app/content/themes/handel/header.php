64867cf697be5304d362b5e3ba43cd3c90a2117c
theandersonn <theandersonn@gmail.com>
2019-08-31 19:24:49 -0300

    Aulas 401 e 402 - header

--- wp-app/content/themes/handel/header.php
+++ wp-app/content/themes/handel/header.php
@@ -9,8 +9,36 @@
 </head>
 <body <?php body_class();?>>
 
-<header class="header">
+<?php
+    $img_url = get_stylesheet_directory_uri() . "/img";
+    $cart_count = WC()->cart->get_cart_contents_count();
+?>
+
+<header class="header container">
     <a href="/">
-        <img src="./img" alt="">
+        <img src="<?= $img_url; ?>/handel.svg" alt="Handel">
     </a>
+
+    <div class="busca">
+        <?php 
+            // Exemplo de função search pelo woocommerce
+            //get_product_search_form();
+        ?>
+
+        <form action="<?php bloginfo("url");?>/loja/" method="get">
+            <input type="text" name="s" id="s" placeholder="Buscar" value="<?php the_search_query(); ?>">
+            <input type="text" name="post_type" value="product" class="hidden">
+            <input type="submit" id="searchbutton" value="Buscar">
+        </form>
+    </div>
+
+    <nav class="conta">
+        <a href="<?= get_permalink( wc_get_page_id( 'myaccount' ) ); ?>" class="minha-conta">Minha Conta</a>
+        <a href="<?= get_permalink( wc_get_page_id( 'cart' ) ); ?>" class="carrinho">
+            Carrinho
+            <?php if($cart_count) { ?>
+                <span class="carrinho-count"><?= $cart_count; ?></span>
+            <?php } ?>    
+        </a>
+    </nav>
 </header>
