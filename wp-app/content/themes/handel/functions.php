a646927887bce70ff129b00fedf5e333ef80d0d6
theandersonn <theandersonn@gmail.com>
2019-09-01 11:12:29 -0300

    Aula 404 Home produtos

--- wp-app/content/themes/handel/functions.php
+++ wp-app/content/themes/handel/functions.php
@@ -22,3 +22,22 @@
         return 6;
     }
     add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
+
+    function handel_product_list($products) { ?>
+        <ul class="products-list">
+            <?php foreach($products as $product) { ?>
+                <li class="product-item">
+                    <a href="<?= $product["link"]; ?>">
+                        <div class="product-info">
+                            <img src="<?= $product["img"]; ?>" alt="<?= $product["name"]; ?>">
+                            <h2><?= $product["name"]; ?> - <span><?= $product["price"]; ?></span></h2>
+                        </div>
+                        <div class="product-overlay">
+                            <span class="btn-link">Ver Mais</span>
+                        </div>
+                    </a>
+                </li>
+            <?php } ?>
+        </ul>
+    <?php    
+    } 
--- wp-app/content/themes/handel/page-home.php
+++ wp-app/content/themes/handel/page-home.php
@@ -3,28 +3,44 @@
 get_header(); ?>
 
 <?php
-
-    $products_slide = wc_get_products([
-        "limit" => 6,
-        "tag" => ["slide",]
-    ]);
-
-    function format_products($products) {
+    function format_products($products, $img_size) {
         $products_final = [];
         
         foreach($products as $product) {
             $products_final[] = [
                 "name" => $product->get_name(),
-                "preco" => $product->get_price_html(),
+                "price" => $product->get_price_html(),
                 "link" => $product->get_permalink(),
-                "img" => wp_get_attachment_image_src($product->get_image_id(), "slide")[0]
+                "img" => wp_get_attachment_image_src($product->get_image_id(), $img_size)[0]
             ];
         }
 
         return $products_final;
     }
 
-    $slide = format_products($products_slide);
+    $products_slide = wc_get_products([
+        "limit" => 6,
+        "tag" => ["slide",]
+    ]);
+
+    $products_new = wc_get_products([
+        "limit" => 9,
+        "orderby" => "date",
+        "order" => "DESC"
+    ]);
+
+    $products_sales = wc_get_products([
+        "limit" => 9,
+        "meta_key" => "total_sales",
+        "orderby" => "meta_value_num",
+        "order" => "DESC"
+    ]);    
+
+    $data = [];
+
+    $data["slide"] = format_products($products_slide, "slide");
+    $data["lancamentos"] = format_products($products_new, "medium");
+    $data["vendidos"] = format_products($products_sales, "medium");    
 ?>
 
 <?php if(have_posts()) { while (have_posts()) { the_post(); ?>
@@ -36,18 +52,28 @@ get_header(); ?>
 
     <section class="slide-wrapper">
         <ul class="slide">
-            <?php foreach($slide as $product) { ?>
+            <?php foreach($data["slide"] as $product) { ?>
                 <li class="slide-item">
                     <img src="<?= $product["img"]; ?>" alt="<?= $product["name"]; ?>">
                     <div class="slide-info">
-                        <span class="slide-preco"><?= $product["preco"]; ?></span>
+                        <span class="slide-preco"><?= $product["price"]; ?></span>
                         <h2 class="slide-nome"><?= $product["name"]; ?></h2>
-                        <a class="slide-link" href="<?= $product["link"]; ?>">Ver Produto</a>
+                        <a class="btn-link" href="<?= $product["link"]; ?>">Ver Produto</a>
                     </div>
                 </li>
             <?php } ?>    
         </ul>
     </section>
+
+    <section class="container">
+        <h1 class="subtitulo">Lançamentos</h1>
+        <?php handel_product_list($data["lancamentos"]); ?>        
+    </section>
+
+    <section class="container">
+        <h1 class="subtitulo">Mais Vendidos</h1>
+        <?php handel_product_list($data["vendidos"]); ?>        
+    </section>    
 <?php } } ?>
 
 <?php get_footer(); ?>
