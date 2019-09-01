8f75fd40bd27b3c6d8ce543c759c102652e970a4
theandersonn <theandersonn@gmail.com>
2019-09-01 12:36:37 -0300

    Aula 405 - Home categorias

--- wp-app/content/themes/handel/page-home.php
+++ wp-app/content/themes/handel/page-home.php
@@ -40,7 +40,28 @@ get_header(); ?>
 
     $data["slide"] = format_products($products_slide, "slide");
     $data["lancamentos"] = format_products($products_new, "medium");
-    $data["vendidos"] = format_products($products_sales, "medium");    
+    $data["vendidos"] = format_products($products_sales, "medium");   
+    
+    $home_id = get_the_ID();
+    $categoria_esquerda = get_post_meta($home_id, "categoria_esquerda", true);
+    $categoria_direita = get_post_meta($home_id, "categoria_direita", true);
+
+    function get_product_category_data($category) {
+        $cat = get_term_by("slug", $category, "product_cat");
+        $cat_id = $cat->term_id;
+        $img_id = get_term_meta($cat_id, "thumbnail_id", true);
+        $img_cat = wp_get_attachment_image_src($img_id, "slide")[0];
+
+        return [
+            "name" => $cat->name,
+            "id" => $cat_id,
+            "link" => get_term_link($cat_id, "product_cat"),
+            "img" => wp_get_attachment_image_src($img_id, "slide")[0]
+        ];
+    }    
+
+    $data["categorias"][$categoria_esquerda] = get_product_category_data($categoria_esquerda);
+    $data["categorias"][$categoria_direita] = get_product_category_data($categoria_direita);
 ?>
 
 <?php if(have_posts()) { while (have_posts()) { the_post(); ?>
@@ -68,6 +89,15 @@ get_header(); ?>
     <section class="container">
         <h1 class="subtitulo">Lançamentos</h1>
         <?php handel_product_list($data["lancamentos"]); ?>        
+    </section>
+
+    <section class="categorias-home">
+        <?php foreach($data["categorias"] as $categoria) { ?>
+            <a href="<?= $categoria["link"];?>">
+                <img src="<?= $categoria["img"];?>" alt="<?= $categoria["name"];?>">
+                <span class="btn-link"><?= $categoria["name"];?></span>
+            </a>
+        <?php } ?>
     </section>
 
     <section class="container">
