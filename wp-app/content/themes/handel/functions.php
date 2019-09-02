d4c03fab33854a2127e1d411ec6d44de642e9217
theandersonn <theandersonn@gmail.com>
2019-09-02 20:29:27 -0300

    Aula 409 - Remover classes default woo

--- wp-app/content/themes/handel/functions.php
+++ wp-app/content/themes/handel/functions.php
@@ -23,6 +23,21 @@
     }
     add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
 
+
+    function remove_some_body_class($classes){
+        $woo_class = array_search("woocommerce", $classes);
+        $woopage_class = array_search("woocommerce-page", $classes);
+        $search = in_array("archive", $classes) || in_array("product-template-default", $classes);
+
+        if ($woo_class && $woopage_class && $search) {
+            unset($classes[$woo_class]);
+            unset($classes[$woopage_class]);
+        }
+
+        return $classes;
+    }
+    add_filter("body_class", "remove_some_body_class");
+
     function format_products($products, $img_size = "medium") {
         $products_final = [];
         
