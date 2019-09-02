19ad8fb43c59aafc4bf944449ee1a19b81deb68a
theandersonn <theandersonn@gmail.com>
2019-09-02 14:34:49 -0300

    Aula 408 - Produtos Filtros

--- wp-app/content/themes/handel/woocommerce/archive-product.php
+++ wp-app/content/themes/handel/woocommerce/archive-product.php
@@ -17,11 +17,55 @@
 
 <article class="container products-archive">
     <nav class="filtros">
-        <h2>Filtros</h2>
+
+        <div class="filtro">
+            <h3 class="filtro-titulo">Categorias</h3>
+            <?php
+                wp_nav_menu([
+                    "menu" => "categorias-interna",
+                    "menu_class" => "filtro-cat",
+                    "container" => false
+                ]);
+            ?>
+        </div>
+
+        <div class="filtro">
+            <?php
+                $attribute_taxonomies = wc_get_attribute_taxonomies();
+
+                foreach($attribute_taxonomies as $attribute) {
+                    the_widget("WC_Widget_Layered_Nav", [
+                        "title" => $attribute->attribute_label,
+                        "attribute" => $attribute->attribute_name
+                    ]);
+                }
+            ?>
+        </div>  
+
+        <div class="filtro">
+            <h3 class="filtro-titulo">Filtrar por preço</h3>
+
+            <form class="filtro-preco">
+                <div>
+                    <label for="min_price">De R$</label>
+                    <input required type="text" name="min_price" id="min_price" value="<?= isset( $_GET['min_price'] ) ? $_GET['min_price'] : ''; ?>">
+                    
+                </div>
+
+                <div>
+                    <label for="max_price">Até R$</label>
+                    <input required type="text" name="max_price" id="max_price" value="<?= isset( $_GET['max_price'] ) ? $_GET['max_price'] : ''; ?>">
+                </div> 
+
+                <button type="submit">Filtrar</button>
+            </form>  
+        </div>          
+
     </nav>
 
     <main>   
         <?php if($data["products"][0]) { ?> 
+            <?php woocommerce_catalog_ordering(); ?>
             <?php handel_product_list($data["products"]); ?>
             <?= get_the_posts_pagination(); ?>
         <?php } else { ?>
